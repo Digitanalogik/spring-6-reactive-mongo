0a5982c24eb4674fd93d713bf20bb61ad637c1e6
Tatu Soininen <taatusti@gmail.com>
2023-09-12 12:18:05 +0300

    Added more service methods: get all, find one, save, update, patch, delete

--- src/main/java/fi/tatu/spring6reactivemongo/services/BeerService.java
+++ src/main/java/fi/tatu/spring6reactivemongo/services/BeerService.java
@@ -1,12 +1,21 @@
 package fi.tatu.spring6reactivemongo.services;
 
 import fi.tatu.spring6reactivemongo.model.BeerDTO;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface BeerService {
 
-    Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDTO);
+    Flux<BeerDTO> listBeers();
 
-    Mono<BeerDTO> getById(String id);
+    Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDTO);
+    Mono<BeerDTO> saveBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> getById(String beerId);
+
+    Mono<BeerDTO> updateBeer(String beerId, BeerDTO beerDTO);
+    Mono<BeerDTO> patchBeer(String beerId, BeerDTO beerDTO);
+
+    Mono<Void> deleteBeerById(String beerId);
 
 }
