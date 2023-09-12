9f2c6766fff26fac01fc9c6d2bf3ce71f7d2f547
Tatu Soininen <taatusti@gmail.com>
2023-09-12 12:02:13 +0300

    Changed to String identifier for MongoDB + added validation annotations

--- src/main/java/fi/tatu/spring6reactivemongo/model/BeerDTO.java
+++ src/main/java/fi/tatu/spring6reactivemongo/model/BeerDTO.java
@@ -1,5 +1,7 @@
 package fi.tatu.spring6reactivemongo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +15,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class BeerDTO {
-    private Integer id;
+    private String id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String beerName;
+
+    @Size(min = 1, max = 255)
     private String beerStyle;
+
     private String upc;
     private Integer quantityOnHand;
     private BigDecimal price;
