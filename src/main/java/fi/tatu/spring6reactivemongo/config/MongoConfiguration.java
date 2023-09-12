b73890f3e0b5d3899f4c3507659e8399fd39468e
Tatu Soininen <taatusti@gmail.com>
2023-09-12 11:16:53 +0300

    Updated domain classes: use string id for MongoDB + added Spring Data annotations

--- src/main/java/fi/tatu/spring6reactivemongo/config/MongoConfiguration.java
+++ src/main/java/fi/tatu/spring6reactivemongo/config/MongoConfiguration.java
@@ -1,13 +1,36 @@
 package fi.tatu.spring6reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 
+import static java.util.Collections.singletonList;
+
 @Configuration
+@ConfigurationProperties(prefix = "spring.data.mongodb")
 public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private Integer port;
+
+    @Value("${spring.data.mongodb.database:sfg}")
+    private String database;
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
 
     @Bean
     public MongoClient mongoClient() {
@@ -16,6 +39,14 @@ public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
 
     @Override
     protected String getDatabaseName() {
-        return "sfg";
+        return this.database;
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential(username, database, password.toCharArray()))
+            .applyToClusterSettings(settings -> settings.hosts(
+                singletonList(new ServerAddress(this.host, this.port)))
+            );
     }
 }
