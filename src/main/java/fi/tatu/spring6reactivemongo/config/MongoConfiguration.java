13ef414e0de7d1f06ec110e3835de1bf71b352ab
Tatu Soininen <taatusti@gmail.com>
2023-09-12 10:31:45 +0300

    Add Mongo database configuration

---
+++ src/main/java/fi/tatu/spring6reactivemongo/config/MongoConfiguration.java
@@ -0,0 +1,21 @@
+package fi.tatu.spring6reactivemongo.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+@Configuration
+public class MongoConfiguration extends AbstractReactiveMongoConfiguration {
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "sfg";
+    }
+}
