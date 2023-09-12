cd25a52e26dbd220ee3bd5210a74471693e9ee07
Tatu Soininen <taatusti@gmail.com>
2023-09-12 10:12:16 +0300

    Initial commit

---
+++ src/main/java/fi/tatu/spring6reactivemongo/Spring6MongoApplication.java
@@ -0,0 +1,13 @@
+package fi.tatu.spring6reactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6MongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6MongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fi/tatu/spring6reactivemongo/Spring6MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tatu.spring6reactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6MongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
