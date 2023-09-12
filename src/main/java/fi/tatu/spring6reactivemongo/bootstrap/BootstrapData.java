12df787c99f0120054af915ad4c7fd1252964b39
Tatu Soininen <taatusti@gmail.com>
2023-09-12 17:18:09 +0300

    Bootstrap sample data

---
+++ src/main/java/fi/tatu/spring6reactivemongo/bootstrap/BootstrapData.java
@@ -0,0 +1,73 @@
+package fi.tatu.spring6reactivemongo.bootstrap;
+
+import fi.tatu.spring6reactivemongo.domain.Beer;
+import fi.tatu.spring6reactivemongo.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        beerRepository.deleteAll()
+                .doOnSuccess(success -> loadBeerData())
+                .subscribe();
+    }
+
+    private void loadBeerData() {
+
+        log.info("Loading Beer data...");
+
+        beerRepository.count().subscribe(count -> {
+            if (count == 0) {
+                Beer beer1 = Beer.builder()
+                        .beerName("Galaxy Cat")
+                        .beerStyle("Pale Ale")
+                        .upc("12356")
+                        .price(new BigDecimal("12.99"))
+                        .quantityOnHand(122)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                Beer beer2 = Beer.builder()
+                        .beerName("Crank")
+                        .beerStyle("Pale Ale")
+                        .upc("12356222")
+                        .price(new BigDecimal("11.99"))
+                        .quantityOnHand(392)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                Beer beer3 = Beer.builder()
+                        .beerName("Sunshine City")
+                        .beerStyle("IPA")
+                        .upc("12356")
+                        .price(new BigDecimal("13.99"))
+                        .quantityOnHand(144)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                // Important! Without subscribe() there would be no backpressure --> nothing would be published
+                // Thus subscribing is necessary to start processing data in the reactive stream
+                beerRepository.save(beer1).subscribe();
+                beerRepository.save(beer2).subscribe();
+                beerRepository.save(beer3).subscribe();
+
+            }
+        });
+    }
+
+}
