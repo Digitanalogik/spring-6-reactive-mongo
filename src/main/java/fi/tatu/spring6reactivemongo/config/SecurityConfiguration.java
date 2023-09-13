e48d0d333cd6c3e7da64bd095ac7e755bd3fa604
Tatu Soininen <taatusti@gmail.com>
2023-09-13 15:37:04 +0300

    Disabled Cross-Site Request Forgery (CSRF)

--- src/main/java/fi/tatu/spring6reactivemongo/config/SecurityConfiguration.java
+++ src/main/java/fi/tatu/spring6reactivemongo/config/SecurityConfiguration.java
@@ -11,12 +11,14 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange()
-                .anyExchange()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+        http
+            .csrf().disable()
+            .authorizeExchange()
+            .anyExchange()
+            .authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
 
         return http.build();
     }
